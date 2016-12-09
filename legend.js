c5cc20e9076a732496e6b47d459e0d51e1fe3c42
yunhan <yunhan@live.unc.edu>
2016-12-08 20:22:49 -0500

    yunhan

---
+++ legend.js
@@ -0,0 +1,134 @@
+///////////////////////////////////////////////////////////////////////////
+//////////////////// Set up and initiate svg containers ///////////////////
+///////////////////////////////////////////////////////////////////////////	
+
+var margin = {
+	top: 0,
+	right: 0,
+	bottom: 0,
+	left: 0
+};
+
+var width = 550,
+	height = 70;
+
+//SVG container
+var svg = d3.select('#trafficAccidents')
+	.append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//Reset the overall font size
+var newFontSize = 70;
+d3.select("html").style("font-size", newFontSize + "%");
+
+///////////////////////////////////////////////////////////////////////////
+//////////////////////////// Draw Heatmap /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+	
+//Based on the heatmap example of: http://blockbuilder.org/milroc/7014412
+
+var colorScale = d3.scale.linear()
+	.domain([0,100])
+	.range(["#EFEFFF","#02386F"])
+	//.interpolate(d3.interpolateHcl);
+
+
+///////////////////////////////////////////////////////////////////////////
+//////////////// Create the gradient for the legend ///////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+//Extra scale since the color scale is interpolated
+var countScale = d3.scale.linear()
+	.domain([0, 100])
+	.range([0, width])
+
+//Calculate the variables for the temp gradient
+var numStops = 10;
+countRange = countScale.domain();
+countRange[2] = countRange[1] - countRange[0];
+countPoint = [];
+for(var i = 0; i < numStops; i++) {
+	countPoint.push(i * countRange[2]/(numStops-1) + countRange[0]);
+}//for i
+
+//Create the gradient
+svg.append("defs")
+	.append("linearGradient")
+	.attr("id", "legend-traffic")
+	.attr("x1", "0%").attr("y1", "0%")
+	.attr("x2", "100%").attr("y2", "0%")
+	.selectAll("stop") 
+	.data(d3.range(numStops))                
+	.enter().append("stop") 
+	.attr("offset", function(d,i) { 
+		return countScale( countPoint[i] )/width;
+	})   
+	.attr("stop-color", function(d,i) { 
+		return colorScale( countPoint[i] ); 
+	});
+
+///////////////////////////////////////////////////////////////////////////
+////////////////////////// Draw the legend ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+var legendWidth = 400;
+//Color Legend container
+var legendsvg = svg.append("g")
+	.attr("class", "legendWrapper")
+	.attr("transform", "translate(" + (width/2) + "," + 30 + ")");
+
+//Draw the Rectangle
+legendsvg.append("rect")
+	.attr("class", "legendRect")
+	.attr("x", -legendWidth/2-30)
+	.attr("y", 0)
+	//.attr("rx", hexRadius*1.25/2)
+	.attr("width", legendWidth)
+	.attr("height", 10)
+	.style("fill", "url(#legend-traffic)");
+
+//Draw the Rectangle for unknown 
+legendsvg.append("rect")
+    .attr("class", "legendRect")
+	.attr("x", 230)
+	.attr("y", 0)
+	.attr("width", 10)
+	.attr("height", 10)
+	.style("fill", "#F5F5F5")
+	.style("stroke", "#DEDEDE")
+    .style("stroke-width", 2)
+	;
+
+legendsvg.append("text")
+    .attr("x",212)
+	.attr("y",28.6)
+	.text("unknown");
+
+//Append title
+legendsvg.append("text")
+	.attr("class", "legendTitle")
+	.attr("x", 0)
+	.attr("y", -10)
+	.style("text-anchor", "middle")
+	.text("Internent Freedom Score");
+
+//Set scale for x-axis
+var xScale = d3.scale.linear()
+	 .range([-legendWidth/2-30, legendWidth/2-30])
+	 .domain([ 0, 100]);
+
+//Define x-axis
+var xAxis = d3.svg.axis()
+	  .orient("bottom")
+	  .ticks(5)
+	  //.tickFormat(formatPercent)
+	  .scale(xScale);
+
+//Set up X axis
+legendsvg.append("g")
+	.attr("class", "axis")
+	.attr("transform", "translate(0," + (10) + ")")
+	.call(xAxis);
